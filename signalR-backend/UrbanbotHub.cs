25ffc2b7ef2255f1336fd20eab4413fd8fbacc4c
Max <max@malein.com>
2022-09-19 13:36:26 +0300

    add backend

---
+++ signalR-backend/UrbanbotHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace signalR_backend
+{
+    public class UrbanbotHub: Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine("Подключено!");
+            return base.OnConnectedAsync();
+        }
+
+        public async Task<string> Echo(string message)
+        {
+            return message;
+        }
+    }
+}
