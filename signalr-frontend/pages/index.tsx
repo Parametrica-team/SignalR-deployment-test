e0b3d2f776f500629374d8233f77ad4e81dab891
Max <max@malein.com>
2022-10-06 08:50:49 +0300

    fetch weather

--- signalr-frontend/pages/index.tsx
+++ signalr-frontend/pages/index.tsx
@@ -1,10 +1,11 @@
 import styles from '../styles/Home.module.css'
-import React from 'react'
+import React, { useEffect } from 'react'
 import useConnection from '../features/connection/useConnection'
 import { useState } from 'react';
 
 export default function Home() {
   const [response, setResponse] = useState('')
+  const [weather, setWeather] = useState('')
 
   let connection = useConnection();
   if (connection){
@@ -12,11 +13,23 @@ export default function Home() {
       setResponse(r);
     })
   }
+
+  useEffect(()=>{
+    const getWeather = async () => {
+      const r = await fetch("https://urbanbot.parametrica.work:5025/weatherforecast");
+      const text = await r.text()
+      setWeather(text)
+    }
+
+    getWeather()
+
+  },[])
   
   return (
     <div className={styles.container}>
       <h1>frontpage</h1>
       <h1>{response}</h1>
+      <div>{weather}</div>
     </div>
   )
 }
